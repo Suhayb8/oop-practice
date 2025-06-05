91e6d2011c938d8f2a063b61f7fe505e71e1afda
Suhyab8 <146775928+Suhayb8@users.noreply.github.com>
2025-06-05 20:14:32 +0100

    Add Animal class with inheritance and polymorphism examples

--- car.py
+++ car.py
@@ -11,3 +11,34 @@ car2 = Car("Bugatti", "BMW")
 
 car1.drive()
 car2.drive()
+print("----animal 🐶")
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def __init__(self,name, age, breed ):
+        super().__init__(name, age)
+        self.breed = breed
+
+    def speak(self):
+        print("-_- 🐶 -_-")
+        print("Woof!")
+        print(self.name, self.age, self.breed)
+
+dog = Dog("🐶John",9,"German Shepherd")
+dog.speak()
+class Cat(Animal):
+    def __init__(self,name, age, breed ):
+        super().__init__(name, age)
+        self.breed = breed
+    def speak(self):
+        print("-_- 🐈 -_-")
+        print("Meow!")
+        print(self.name, self.age, self.breed)
+
+cat = Cat("🐈lucky", 7, "Sphynx")
+cat.speak()
