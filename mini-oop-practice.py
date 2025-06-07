acc624a933c26d9f4bd981dc2249dc502495c63a
Suhyab8 <146775928+Suhayb8@users.noreply.github.com>
2025-06-07 19:54:47 +0100

    Add shapes.py to calculate area of Circle, Rectangle, and Triangle using OOP principles: inheritance, encapsulation, polymorphism, and abstraction

---
+++ mini-oop-practice.py
@@ -0,0 +1,3 @@
+
+class Order:
+    def item(self):
---
+++ shapes.py
@@ -0,0 +1,35 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius ** 2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.width
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    def area(self):
+        return 0.5 * self.base * self.height
+
+shapes =[
+    Circle(6),
+    Rectangle(8, 2),
+    Triangle(10, 2)
+]
+for shape in shapes:
+    print(f" {shape.__class__.__name__} area: {shape.area()}")
+
+
